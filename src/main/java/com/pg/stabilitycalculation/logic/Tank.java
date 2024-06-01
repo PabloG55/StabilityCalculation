a8c281c9f6df2c6a8e6e19ff50466a61523db5bc
Pablo Garces <89934775+PabloG55@users.noreply.github.com>
2024-06-01 12:33:13 -0400

    added an igu

--- src/main/java/com/pg/stabilitycalculation/logic/Tank.java
+++ src/main/java/com/pg/stabilitycalculation/logic/Tank.java
@@ -1,47 +1,5 @@
 package com.pg.stabilitycalculation.logic;
-import java.sql.*;
-;
+
 public class Tank {
-    public static void main(String[] args) {
-        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/endeavourII_stability",
-                System.getenv("DB_USERNAME"), System.getenv("DB_PASSWORD"))) {
-            // Establish a connection to your database
-            
-            // Define the tankValue
-            double tankValue = 10;
-            double totalTanksMOMV2 = CalculationUtils.calculateTotalMOMV2(connection, tankValue, TankConstants.ALL_TANKS);
-            double totalDataMOMV2 = CalculationUtils.getTotalShipDataMOMV2(connection);
-            double totalShipMOMV2 = CalculationUtils.calculateTotalShipMOMV2(totalDataMOMV2, totalTanksMOMV2);
-
-            double totalTanksMOMV1 = CalculationUtils.calculateTotalMOMV1(connection, tankValue, TankConstants.ALL_TANKS);
-            double totalDataMOMV1 = CalculationUtils.getTotalShipDataMOMV1(connection);
-            double totalShipMOMV1 = CalculationUtils.calculateTotalShipMOMV1(totalDataMOMV1, totalTanksMOMV1);
-
-            double totalTanksWeight = CalculationUtils.calculateTotalWeight(connection, tankValue, TankConstants.ALL_TANKS);
-            double totalDataWeight = CalculationUtils.getTotalShipDataWeight(connection);
-            double totalShipWeight = CalculationUtils.calculateTotalShipWeight(totalDataWeight, totalTanksWeight);
-            double totalFS = CalculationUtils.calculateTotalFS(connection, tankValue, TankConstants.ALL_TANKS);
-            double gg = CalculationUtils.calculateGG(totalShipWeight, totalFS);
-            double kg = CalculationUtils.calculateKG(totalShipWeight, totalShipMOMV2, gg);
-            double lcg = CalculationUtils.calculateTotalShipLCG(totalShipWeight, totalShipMOMV1);
-            double lcb = CalculationUtils.calculateLCB(connection, totalShipWeight);
-            double kml = CalculationUtils.calculateKML(connection, totalShipWeight);
-            double lcf = CalculationUtils.calculateLCF(connection, totalShipWeight);
-            double tLevel = CalculationUtils.calculateTLevel(lcb, lcg, kml, kg);
-            double dc = CalculationUtils.calculateDC(connection, totalShipWeight);
-            double ta = CalculationUtils.calculateTa(dc, tLevel, lcf);
-            double kmt = CalculationUtils.calculateKMT(connection, totalShipWeight);
-
-            
-            
-            System.out.println(CalculationUtils.calculateGM(kmt, kg));
-
-            
-
-        
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+    
 }
